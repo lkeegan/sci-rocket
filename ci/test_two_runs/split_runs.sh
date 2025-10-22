53872df210f0630171f8ff8cb6b5367cadd76c95
Liam Keegan <liam@keegan.ch>
2025-10-22 17:48:50 +0200

    Add test of multiple fastq runs

--- ci/test_two_runs/split_runs.sh
+++ ci/test_two_runs/split_runs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # splits the reads from folder fastq into two folders, run1 and run2,
-# each with half of the reads, so that combining the two gives the same reads as using the fast folder.
+# each with half of the reads, so that combining the two gives the same reads as using the fastq folder.
 
-NFULL=$(zcat fastq/Undetermined_S0_R1_001.fastq.gz | wc -l)
+NFULL=$(zcat ../fastq/Undetermined_S0_R1_001.fastq.gz | wc -l)
 N=$(($NFULL / 2))
 
 mkdir -p run1
@@ -13,7 +13,7 @@ rm -rf run2/*
 
 for R in "1" "2"
 do
-    INFILE="fastq/Undetermined_S0_R${R}_001.fastq.gz"
+    INFILE="../fastq/Undetermined_S0_R${R}_001.fastq.gz"
 
     OUTFILE="run1/Undetermined_S0_R${R}_001.fastq"
     rm -f "${OUTFILE}.gz"
